d0835c500a6a11b9035347323a8b81f25c935ce9
sohyun <shparkso162@gmail.com>
2021-07-25 14:54:56 +0900

    Redirecting

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,14 @@
 import React from "react";
+import "./About.css";
 
-function About() {
-    return <span>About this page</span>
+function About(props) {
+    return (
+        <div className="about__container">
+            <span>
+                박소현
+            </span>
+            <span>2021-07-25</span>
+        </div>
+    );
 }
 export default About;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -51,4 +51,4 @@ class Home extends React.Component {
           );
   }
 }
-export default App;
+export default Home;
